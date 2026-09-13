6f56933791298f7581519ca0f50607d79a3e3680
goran444 <60275602+goran444@users.noreply.github.com>
2026-09-13 22:19:26 +0200

    Refactor generate_license function to use constant hwid
    Simplify license generation by setting hwid to a constant value.

--- license.py
+++ license.py
@@ -1,16 +1,7 @@
-
 def generate_license(hardware_id):
-    try:
-        # Zadržavamo samo cifre iz bilo kog serijskog broja (bilo da ima PE, AB ili druga slova)
-        digits_only = ''.join(filter(str.isdigit, str(hardware_id)))
-        
-        if not digits_only:
-            hwid = 5432
-        else:
-            hwid = int(digits_only)
-    except (ValueError, TypeError):
-        hwid = 5432
-
-    # Matematika potpuno identična GPC skripti
+    # Za objavljenu v1.0 GPC skriptu na Zen Library, 
+    # server vraća tačan licencni kod koji uređaj očekuje.
+    hwid = 5432
     license_code = (hwid % 9000) + 1000
+    
     return license_code
