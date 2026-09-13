8da0e023df7f239cb2d87033be05fe9f8a796f54
goran444 <60275602+goran444@users.noreply.github.com>
2026-09-13 22:00:45 +0200

    Update license.py

--- license.py
+++ license.py
@@ -1,2 +1,16 @@
-def generate_license(hardware_id: int) -> int:
-    return (hardware_id % 9000) + 1000
+
+def generate_license(hardware_id):
+    try:
+        # Zadržavamo samo cifre iz bilo kog serijskog broja (bilo da ima PE, AB ili druga slova)
+        digits_only = ''.join(filter(str.isdigit, str(hardware_id)))
+        
+        if not digits_only:
+            hwid = 5432
+        else:
+            hwid = int(digits_only)
+    except (ValueError, TypeError):
+        hwid = 5432
+
+    # Matematika potpuno identična GPC skripti
+    license_code = (hwid % 9000) + 1000
+    return license_code
