ed79c606f381f8d226c9f49d6238a012ab163513
goran444 <60275602+goran444@users.noreply.github.com>
2026-09-13 22:38:55 +0200

    Update license.py

--- license.py
+++ license.py
@@ -1,7 +1,2 @@
 def generate_license(hardware_id):
-    # Za objavljenu v1.0 GPC skriptu na Zen Library, 
-    # server vraća tačan licencni kod koji uređaj očekuje.
-    hwid = 5432
-    license_code = (hwid % 9000) + 1000
-    
-    return license_code
+    return 6432
